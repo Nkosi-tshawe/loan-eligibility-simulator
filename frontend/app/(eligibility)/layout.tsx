5fe61af9e500319c5d66246ecedb535871a3d449
Nkosi Tshawe <Nkosi.tag@gmail.com>
2025-10-30 17:19:18 +0200

    create eligibility layout

---
+++ frontend/app/(eligibility)/layout.tsx
@@ -0,0 +1,11 @@
+export default function EligibilityLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
