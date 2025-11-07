f4657b1b33e2d761320087a6a2e3ab4d9407fc50
Nkosi Tshawe <Nkosi.tag@gmail.com>
2025-11-07 11:52:11 +0200

    install next-int for translation

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,14 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
   /* config options here */
-  output: 'standalone',
+  output: 'standalone' as const,
   env: {
     NEXT_PUBLIC_API_URL: process?.env?.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api',
     NEXT_ENV: process?.env?.NEXT_ENV || 'development',
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
